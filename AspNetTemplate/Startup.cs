4270d37fdd7a3b4823f273910a273dfcc623eefd
Emre Mumcu <1emremumcu@gmail.com>
2021-10-02 22:36:19 +0300

    updated

--- AspNetTemplate/Startup.cs
+++ AspNetTemplate/Startup.cs
@@ -8,12 +8,32 @@ namespace AspNetTemplate
 
     public class Startup
     {
+        /// <summary>
+        /// When the application is requested for the first time, it calls ConfigureServices method. 
+        /// ASP.net core has built-in support for Dependency Injection. We can add services to DI container using this method.
+        /// Use ConfigureServices method to configure Dependency Injection (services).
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services
+                .AddControllersWithViews()
+                // Install-Package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+                .AddRazorRuntimeCompilation(); 
+            
             services.AddRazorPages();
+
+            services.AddHttpContextAccessor();
         }
 
+        /// <summary>
+        /// This method is used to define how the application will respond on each HTTP request.
+        /// This method is also used to configure middleware in HTTP pipeline.
+        /// Use Configure method to set up middlewares, routing rules etc.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,3 +60,36 @@ namespace AspNetTemplate
         }
     }
 }
+
+// ------------------------------------------------------------------------------------------------------------
+// The following Startup.Configure method adds security-related middleware components in the recommended order:
+// ------------------------------------------------------------------------------------------------------------
+
+//public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//{
+//    if (env.IsDevelopment()) {
+//        app.UseDeveloperExceptionPage();
+//        app.UseDatabaseErrorPage();
+//    }
+//    else {
+//        app.UseExceptionHandler("/Error");
+//        app.UseHsts();
+//    }
+//    app.UseHttpsRedirection();
+//    app.UseStaticFiles();
+//    app.UseCookiePolicy();
+//    app.UseRouting();
+//    app.UseRequestLocalization();
+//    app.UseCors();
+//    app.UseAuthentication();
+//    app.UseAuthorization();
+//    app.UseSession();
+//    app.UseResponseCaching();
+//    app.UseEndpoints(endpoints =>
+//    {
+//        endpoints.MapRazorPages();
+//        endpoints.MapControllerRoute(
+//            name: "default",
+//            pattern: "{controller=Home}/{action=Index}/{id?}");
+//    });
+//}
